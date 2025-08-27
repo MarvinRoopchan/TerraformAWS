688461e9f4690cfaa0b4d6a59915d25f39fd14d6
Marvin Roopchan <marvinroopchan@Marvins-Air.localdomain>
2025-08-27 16:29:28 -0400

    Add configuration

---
+++ main.tf
@@ -0,0 +1,127 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#~ --- S3 Bucket ---
+resource "aws_s3_bucket" "site" {
+  bucket = "marvin-launchpad2"
+  tags = {
+    Name = "marvin-launchpad2"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket                  = aws_s3_bucket.site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_index" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadIndexOnly"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.site.arn}/index.html"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.site]
+}
+
+
+# Enable static website hosting
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Lifecycle rule
+resource "aws_s3_bucket_lifecycle_configuration" "site_lifecycle" {
+  bucket = aws_s3_bucket.site.id
+
+  rule {
+    id     = "GlacierTransitionsAndExpire"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+
+    transition {
+      days          = 180
+      storage_class = "DEEP_ARCHIVE"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
+
+#~ --- CloudFront Distribution ---
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled             = true
+  default_root_object = "index.html"
+  comment             = "CloudFront distribution for marvin-launchpad2 static site"
+  tags = {
+    Name = "s3-website-marvin-launchpad"
+  }
+
+  origin {
+    domain_name = aws_s3_bucket.site.website_endpoint
+    origin_id   = "s3-website-marvin-launchpad"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-website-marvin-launchpad"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
